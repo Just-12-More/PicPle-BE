34bdcbe123d7db3d03ba14f8e4c9b74ae5e89b7d
blueoxygens <exboxer234@gmail.com>
2025-04-10 23:48:04 +0900

    [chore] 프로젝트 기본 세팅

---
+++ PicPle/src/main/java/com/Just_112_More/PicPle/PicPleApplication.java
@@ -0,0 +1,13 @@
+package com.Just_112_More.PicPle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPleApplication.class, args);
+	}
+
+}
---
+++ PicPle/src/test/java/com/Just_112_More/PicPle/PicPleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Just_112_More.PicPle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
