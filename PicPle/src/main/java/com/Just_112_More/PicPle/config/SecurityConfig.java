831874020b1b7f8f9ad10a9f48515596bfc3559d
rudalsss <linda284@naver.com>
2025-05-04 00:51:48 +0900

    [ chore ] lombok, validation, devtools, 테스트용 H2DB dependencies 추가

--- PicPle/src/main/java/com/Just_112_More/PicPle/config/SecurityConfig.java
+++ PicPle/src/main/java/com/Just_112_More/PicPle/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -15,8 +16,11 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
                         .anyRequest().permitAll()
-                );
+                )
+                .headers(headers -> headers.disable());
+
         return http.build();
     }
 }
