50c790a0d24171a7deed7dc41c2813a7ed64b00e
rudalsss <linda284@naver.com>
2025-05-07 15:54:08 +0900

    [feat] User도메인 패키지 및 엔티티 생성

---
+++ PicPle/src/main/java/com/Just_112_More/PicPle/user/domain/User.java
@@ -0,0 +1,58 @@
+package com.Just_112_More.PicPle.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@ToString(exclude = "userLikes")
+public class User {
+    @Id @GeneratedValue
+    @Column(name="user_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private LoginProvider provider;
+
+    private String userName;
+
+    private String userImageUrl;
+
+    private boolean isDeleted = false;
+
+    private LocalDateTime userCreate;
+
+    private LocalDateTime deletedAt;
+
+    @OneToMany(mappedBy="user", cascade = CascadeType.REMOVE)
+    private List<Photo> userPhotos = new ArrayList<>();
+
+    @OneToMany(mappedBy="user", cascade = CascadeType.REMOVE)
+    private List<Like> userLikes = new ArrayList<>();
+
+    // 엔티티가 저장되기 전에 userCreate 자동 설정
+    @PrePersist
+    public void prePersist() {
+        if (this.userCreate == null) {
+            this.userCreate = LocalDateTime.now();
+        }
+    }
+
+    //논리 삭제 메서드
+    public void deleteUser() {
+        if (this.isDeleted) {
+            throw new IllegalStateException("User is already deleted");
+        }
+        this.isDeleted = true;
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public boolean isValid() {
+        return !isDeleted || (deletedAt != null && deletedAt.isBefore(LocalDateTime.now()));
+    }
+
+}
