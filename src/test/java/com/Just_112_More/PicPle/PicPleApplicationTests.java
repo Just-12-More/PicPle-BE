b52827b08a7f1dce0eaa8bc71ee888ba6c83a8fc
blueoxygens <exboxer234@gmail.com>
2025-05-22 21:02:03 +0900

    fifeaxest ìt dependency 추가

--- src/test/java/com/Just_112_More/PicPle/PicPleApplicationTests.java
+++ src/test/java/com/Just_112_More/PicPle/PicPleApplicationTests.java
@@ -2,6 +2,8 @@ package com.Just_112_More.PicPle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
 @ActiveProfiles("test")
