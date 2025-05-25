0e69188e95e3a442dd6538850d459b5d8f7a645d
rudalsss <linda284@naver.com>
2025-05-25 23:32:15 +0900

    [feat] Api 공통응답 클래스 ( ApiResponse, ApiError ) 추가

---
+++ src/main/java/com/Just_112_More/PicPle/common/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.Just_112_More.PicPle.common;
+
+import lombok.Builder;
+
+public class ApiResponse <T>{
+    private boolean isSuccess;
+    private T data;
+    private ApiError error; // 성공시 null
+
+    @Builder
+    private ApiResponse(String path, T data, Boolean isSuccess, ApiError error ){
+        this.isSuccess = isSuccess;
+        this.data = data;
+        this.error = error;
+    }
+
+    private static <T> ApiResponse<T> success(T data){
+        return ApiResponse.<T>builder()
+                .isSuccess(true)
+                .data(data)
+                .error(null)
+                .build();
+    }
+
+    private static <T> ApiResponse<T> fail(T data, String code, String message){
+        return ApiResponse.<T>builder()
+                .isSuccess(false)
+                .data(data)
+                .error(new ApiError(code, message))
+                .build();
+    }
+}
