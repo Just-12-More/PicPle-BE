41761bbb88ca0bf08b2c49d12ba54b4cfda43e1b
rudalsss <linda284@naver.com>
2025-05-31 00:46:35 +0900

    [refactor] AuthController API들 BaseResponse수정 & 커스텀예외생성 및 처리로직 구현

--- src/main/java/com/Just_112_More/PicPle/exception/ErrorCode.java
+++ src/main/java/com/Just_112_More/PicPle/exception/ErrorCode.java
@@ -8,13 +8,33 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    // auth error
+    // spring security exception
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근권한이 없습니다."),
     NOT_LOGIN_USER(HttpStatus.FORBIDDEN, "로그인하지 않은 사용자입니다."),
-    INVALID_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-    EXPIRED_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
-    TOKEN_CORRECT_ERROR(HttpStatus.UNAUTHORIZED, "토큰이 올바르지 않습니다."),
-    TOKEN_GENERATION_ERROR(HttpStatus.UNAUTHORIZED, "토큰생성에 실패하였습니다.");
+
+    // user exception
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
+
+    // jwt 내부 에러
+    INVALID_TOKEN_SIGNATURE(HttpStatus.FORBIDDEN, "JWT 서명 검증 실패"),
+    MALFORMED_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "잘못된 JWT 형식입니다."),
+    UNSUPPORTED_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "지원하지 않는 JWT입니다."),
+    EMPTY_TOKEN_ERROR(HttpStatus.BAD_REQUEST, "토큰이 비어있습니다."),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    TOKEN_PARSING_FAILED(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰 - 파싱실패"),
+
+    // jwt 인증에러
+    ACCESS_TOKEN_MISSING(HttpStatus.UNAUTHORIZED, "Access Token이 요청에 포함되지 않았습니다."),
+//    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
+//    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 Access Token입니다."),
+    MISMATCHED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "저장된 리프레시 토큰과 일치하지 않습니다."),
+
+    // oauth login 관련 에러
+    INVALID_OAUTH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 SDK code입니다."),
+    OAUTH_SERVER_ERROR(HttpStatus.BAD_GATEWAY, "OAuth 인증 서버에 연결할 수 없습니다."),
+    OAUTH_RESPONSE_INVALID(HttpStatus.INTERNAL_SERVER_ERROR, "OAuth 사용자 정보 응답이 잘못되었습니다."),
+    REDIS_SAVE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "리프레시 토큰 저장에 실패했습니다.")
+    ;
 
     private final HttpStatus status;
     private final String message;
