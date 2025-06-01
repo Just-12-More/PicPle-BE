cb5ed7982d557ae6d932b04dfaa9bc6bc69d7b99
blueoxygens <exboxer234@gmail.com>
2025-06-02 01:56:00 +0900

    [fix] s3 url 인증 오류 해결

--- src/main/java/com/Just_112_More/PicPle/photo/controller/S3Controller.java
+++ src/main/java/com/Just_112_More/PicPle/photo/controller/S3Controller.java
@@ -1,10 +1,14 @@
 package com.Just_112_More.PicPle.photo.controller;
 
 import com.Just_112_More.PicPle.common.ApiResponse;
+import com.Just_112_More.PicPle.exception.CustomException;
+import com.Just_112_More.PicPle.exception.ErrorCode;
 import com.Just_112_More.PicPle.photo.dto.GetS3UrlDto;
 import com.Just_112_More.PicPle.photo.service.S3Service;
+import com.Just_112_More.PicPle.security.jwt.JwtUtil;
 import com.Just_112_More.PicPle.user.domain.AuthUser;
 import com.Just_112_More.PicPle.user.domain.User;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +22,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/s3")
 public class S3Controller {
     private final S3Service s3Service;
+    private final JwtUtil jwtUtil;
 
     @GetMapping("/posturl")
     public ResponseEntity<ApiResponse<GetS3UrlDto>> getPostS3Url(
-            @AuthUser User user,
+            //@AuthUser User user,
+            HttpServletRequest request,
             @RequestParam String filename) {
         try {
-            GetS3UrlDto dto = s3Service.getPostS3Url(user.getId(), filename);
+            String token = jwtUtil.resolveToken(request);
+            if (token == null) {
+                throw new CustomException(ErrorCode.ACCESS_TOKEN_MISSING);
+            }
+            jwtUtil.validateAccessToken(token);
+            Long userId = jwtUtil.extractUserId(token, false);
+            GetS3UrlDto dto = s3Service.getPostS3Url(userId, filename);
             return ResponseEntity.ok(ApiResponse.success(dto)); // 성공 응답
         } catch (Exception e) {
             return ResponseEntity
@@ -35,10 +47,17 @@ public class S3Controller {
 
     @GetMapping("/geturl")
     public ResponseEntity<ApiResponse<GetS3UrlDto>> getGetS3Url(
-            @AuthUser User user,
+            //@AuthUser User user,
+            HttpServletRequest request,
             @RequestParam String filename) {
         try {
-            GetS3UrlDto dto = s3Service.getGetS3Url(user.getId(), filename);
+            String token = jwtUtil.resolveToken(request);
+            if (token == null) {
+                throw new CustomException(ErrorCode.ACCESS_TOKEN_MISSING);
+            }
+            jwtUtil.validateAccessToken(token);
+            Long userId = jwtUtil.extractUserId(token, false);
+            GetS3UrlDto dto = s3Service.getGetS3Url(userId, filename);
             return ResponseEntity.ok(ApiResponse.success(dto)); // 성공 응답
         } catch (Exception e) {
             return ResponseEntity
