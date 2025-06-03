64f292a8adfffa4f7fc6be579cd40809e4d0e4b7
blueoxygens <exboxer234@gmail.com>
2025-06-03 21:54:34 +0900

    [feat] 인접 반경 사진 반환 추가

---
+++ src/main/java/com/Just_112_More/PicPle/photo/dto/photoSearchRequestDto.java
@@ -0,0 +1,11 @@
+package com.Just_112_More.PicPle.photo.dto;
+
+import lombok.Getter;
+
+@Getter
+public class photoSearchRequestDto {
+    private Double latitude;
+    private Double longitude;
+    private Double radius;
+}
+
