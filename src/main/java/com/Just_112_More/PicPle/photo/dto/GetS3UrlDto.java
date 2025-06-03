210435bee9c497433de827c786a45d5357206c90
blueoxygens <exboxer234@gmail.com>
2025-06-03 14:17:40 +0900

    [fix] DTO에 getter 추가

--- src/main/java/com/Just_112_More/PicPle/photo/dto/GetS3UrlDto.java
+++ src/main/java/com/Just_112_More/PicPle/photo/dto/GetS3UrlDto.java
@@ -1,12 +1,14 @@
 package com.Just_112_More.PicPle.photo.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Builder
+@Getter
 public class GetS3UrlDto {
     private String preSignedUrl;
     private String key;
 
-    @Builder
     public GetS3UrlDto(String preSignedUrl, String key) {
         this.preSignedUrl = preSignedUrl;
         this.key = key;
