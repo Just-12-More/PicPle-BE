21ea8a665f4518dcb1f25ecf216497e8bb812429
blueoxygens <exboxer234@gmail.com>
2025-06-03 13:09:03 +0900

    [fix] s3 presigned url 에러 수정

--- src/main/java/com/Just_112_More/PicPle/photo/controller/S3Controller.java
+++ src/main/java/com/Just_112_More/PicPle/photo/controller/S3Controller.java
@@ -4,18 +4,16 @@ import com.Just_112_More.PicPle.common.ApiResponse;
 import com.Just_112_More.PicPle.exception.CustomException;
 import com.Just_112_More.PicPle.exception.ErrorCode;
 import com.Just_112_More.PicPle.photo.dto.GetS3UrlDto;
+import com.Just_112_More.PicPle.photo.dto.GetS3UrlRequest;
 import com.Just_112_More.PicPle.photo.service.S3Service;
 import com.Just_112_More.PicPle.security.jwt.JwtUtil;
-import com.Just_112_More.PicPle.user.domain.AuthUser;
-import com.Just_112_More.PicPle.user.domain.User;
+//import com.Just_112_More.PicPle.user.domain.AuthUser;
+//import com.Just_112_More.PicPle.user.domain.User;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,11 +22,10 @@ public class S3Controller {
     private final S3Service s3Service;
     private final JwtUtil jwtUtil;
 
-    @GetMapping("/posturl")
-    public ResponseEntity<ApiResponse<GetS3UrlDto>> getPostS3Url(
-            //@AuthUser User user,
+    @PostMapping("/posturl")
+    public ResponseEntity<ApiResponse<?>> getPostS3Url(
             HttpServletRequest request,
-            @RequestParam String filename) {
+            @RequestBody GetS3UrlRequest s3Request) {
         try {
             String token = jwtUtil.resolveToken(request);
             if (token == null) {
@@ -36,8 +33,14 @@ public class S3Controller {
             }
             jwtUtil.validateAccessToken(token);
             Long userId = jwtUtil.extractUserId(token, false);
+
+            String filename = s3Request.getFilename();
             GetS3UrlDto dto = s3Service.getPostS3Url(userId, filename);
-            return ResponseEntity.ok(ApiResponse.success(dto)); // 성공 응답
+            return ResponseEntity.ok(ApiResponse.success(dto));
+        } catch (CustomException e) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(ApiResponse.fail(null, e.getErrorCode().name(), e.getMessage()));
         } catch (Exception e) {
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -45,25 +48,34 @@ public class S3Controller {
         }
     }
 
-    @GetMapping("/geturl")
-    public ResponseEntity<ApiResponse<GetS3UrlDto>> getGetS3Url(
-            //@AuthUser User user,
+
+    @PostMapping("/geturl")
+    public ResponseEntity<ApiResponse<?>> getGetS3Url(
             HttpServletRequest request,
-            @RequestParam String filename) {
+            @RequestBody GetS3UrlRequest s3Request) {
         try {
             String token = jwtUtil.resolveToken(request);
             if (token == null) {
                 throw new CustomException(ErrorCode.ACCESS_TOKEN_MISSING);
             }
+
             jwtUtil.validateAccessToken(token);
             Long userId = jwtUtil.extractUserId(token, false);
+
+            String filename = s3Request.getFilename();
             GetS3UrlDto dto = s3Service.getGetS3Url(userId, filename);
-            return ResponseEntity.ok(ApiResponse.success(dto)); // 성공 응답
+
+            return ResponseEntity.ok(ApiResponse.success(dto));
+        } catch (CustomException e) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(ApiResponse.fail(null, e.getErrorCode().name(), e.getMessage()));
         } catch (Exception e) {
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(ApiResponse.fail(null, "INTERNAL_ERROR", "서버 오류가 발생했습니다."));
         }
     }
+
 
 }
---
+++ src/main/java/com/Just_112_More/PicPle/photo/dto/GetS3UrlRequest.java
@@ -0,0 +1,9 @@
+package com.Just_112_More.PicPle.photo.dto;
+
+import lombok.Getter;
+
+@Getter
+public class GetS3UrlRequest {
+    private String filename;
+}
+
