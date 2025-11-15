6779a1ad92e3c8902c0b91b78921b4a9928a6d08
rudalsss <linda284@naver.com>
2025-11-15 21:07:16 +0900

    [feat] 시스템구동시 top10리스트 레디스 캐시에 저장

---
+++ src/main/java/com/Just_112_More/PicPle/stat/service/HotPlaceService.java
@@ -0,0 +1,43 @@
+package com.Just_112_More.PicPle.stat.service;
+
+import com.Just_112_More.PicPle.exception.CustomException;
+import com.Just_112_More.PicPle.exception.ErrorCode;
+import com.Just_112_More.PicPle.stat.dto.HotPlaceResponseList;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class HotPlaceService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+    private final ObjectMapper objectMapper;
+    private final SimpMessagingTemplate messagingTemplate;
+    private final LocationStatService locationStatService;
+    private static final String TOP10_KEY = "top10::hotplaces";
+
+    public void saveTop10Cache(HotPlaceResponseList top10LocationStats) {
+        try{
+            String json = objectMapper.writeValueAsString(top10LocationStats);
+            redisTemplate.opsForValue().set(TOP10_KEY, json);
+        } catch (Exception e){
+            throw new CustomException(ErrorCode.REDIS_SAVE_FAIL);
+        }
+    }
+
+    public HotPlaceResponseList getTop10Cache(){
+        String json = redisTemplate.opsForValue().get(TOP10_KEY);
+        if (json == null) return null;
+        try{
+            return objectMapper.readValue(json, HotPlaceResponseList.class);
+        } catch (Exception e){
+            throw new CustomException(ErrorCode.REDIS_READ_FAIL);
+        }
+    }
+
+}
