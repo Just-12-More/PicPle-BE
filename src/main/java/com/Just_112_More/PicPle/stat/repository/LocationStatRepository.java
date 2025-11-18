59f8cd5df00da0325556823c42c2ed0dd0c9a185
rudalsss <linda284@naver.com>
2025-11-19 00:25:58 +0900

    [refactor] @Modifying해결

--- src/main/java/com/Just_112_More/PicPle/stat/repository/LocationStatRepository.java
+++ src/main/java/com/Just_112_More/PicPle/stat/repository/LocationStatRepository.java
@@ -19,7 +19,8 @@ public interface LocationStatRepository extends JpaRepository<LocationStat, Long
 
     Optional<LocationStat> findByLocationLabel(String locationLabel);
 
-    @Modifying
+    @Modifying(clearAutomatically = true)
+    @Transactional
     @Query(value = """
         INSERT INTO location_stat (location_label, road_address, photo_cnt, last_update_time)
         VALUES (:locationLabel, :roadAddress, 1, NOW())
@@ -27,7 +28,7 @@ public interface LocationStatRepository extends JpaRepository<LocationStat, Long
         photo_cnt = photo_cnt + 1,
         last_update_time = NOW()
     """, nativeQuery = true)
-    void upsertStat(
+    int upsertStat(
             @Param("locationLabel") String locationLabel,
             @Param("roadAddress") String roadAddress
     );
