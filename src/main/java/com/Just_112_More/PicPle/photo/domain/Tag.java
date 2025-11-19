e30b8420b3cc53a0a2118399ae889ef970b43281
lwamuhaji <lwamuhaji@gmail.com>
2025-11-19 15:48:50 +0900

    [fix] tag 테이블 오타 수정

--- src/main/java/com/Just_112_More/PicPle/photo/domain/Tag.java
+++ src/main/java/com/Just_112_More/PicPle/photo/domain/Tag.java
@@ -14,7 +14,7 @@ import lombok.ToString;
         uniqueConstraints = {
             @UniqueConstraint(
                     name= "UK_tag_name_type",
-                    columnNames = {"name", "type"}
+                    columnNames = {"name", "tag_type"}
             )
         },
         indexes = {
