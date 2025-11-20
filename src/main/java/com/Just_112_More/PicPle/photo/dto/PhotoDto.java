5b68cbb4bf6eb93eb9fc616177b98a275cd3b1d2
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2025-11-21 00:12:08 +0900

    [fix] s3 url 접두사 추가

--- src/main/java/com/Just_112_More/PicPle/photo/dto/PhotoDto.java
+++ src/main/java/com/Just_112_More/PicPle/photo/dto/PhotoDto.java
@@ -4,19 +4,23 @@ import com.Just_112_More.PicPle.photo.domain.Photo;
 import com.Just_112_More.PicPle.photo.domain.PhotoTag;
 import com.Just_112_More.PicPle.photo.domain.Tag;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
 public class PhotoDto {
+    @Value("${urls.s3}")
+    private String s3Url;
+
     private final Long id;
     private final String imgUrl;
     private final List<String> tags;
 
     public PhotoDto(Photo photo) {
         this.id = photo.getId();
-        this.imgUrl = photo.getPhotoUrl();
+        this.imgUrl = s3Url + photo.getPhotoUrl();
         this.tags = photo.getPhotoTags().stream()
                 .map(PhotoTag::getTag)
                 .map(Tag::getName)
